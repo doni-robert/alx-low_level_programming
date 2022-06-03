cafb41054ae453e60cecb9152235d4ec6eaa8383
doni-robert <irobertndungu@gmail.com>
2022-06-02 19:39:42 -0700

    fixed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ *prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ */
 int main(void)
 {
-  puts ("Programming is like building a multilingual puzzle");
-  return (0);
-  }
+	puts ("Programming is like building a multilingual puzzle");
+	return (0);
+}
