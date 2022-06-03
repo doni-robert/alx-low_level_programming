83bd0fb9afc8504b5a2604633000bebcd656ef96
doni-robert <irobertndungu@gmail.com>
2022-06-02 19:44:25 -0700

    fixed betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
- *prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ *prints exactly 
+ *Programming is like building a multilingual puzzle
+ * followed by a new line
+ *main-Entry point
+ *Return: 0 if unsuccessful
  */
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
