aaf30d09d0069c356d123bb245cc3c26f947fa2f
doni-robert <irobertndungu@gmail.com>
2022-06-04 01:03:51 -0700

    fixed betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- *prints exactly 
- *Programming is like building a multilingual puzzle
- * followed by a new line
  *main-Entry point
+ * 
  *Return: 0 if unsuccessful
  */
 int main(void)
