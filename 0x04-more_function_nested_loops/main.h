77a46b35ca247541c20d8ef08160cf421d076171
doni-robert <irobertndungu@gmail.com>
2022-06-09 17:13:18 -0700

    task 0 corrected

--- 0x04-more_function_nested_loops/main.h
+++ 0x04-more_function_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_h
 /**
-* Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
- */
+* Description: Header file containing prototypes for all functions
+* used in the 0x03-more_functions_nested_loops directory.
+*/
 
-int _putchar(char c)
+int _isupper(int c);
+
+#endif
