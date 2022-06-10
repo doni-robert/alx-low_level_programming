6bf930f0b9ca3f4b584fc51580ed9791ef8b7f23
doni-robert <irobertndungu@gmail.com>
2022-06-09 17:02:30 -0700

    task 0 done

---
+++ 0x04-more_function_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_h
+/**
+* Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c)
