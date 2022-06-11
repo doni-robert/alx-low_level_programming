1c8de4cab0b99294813eabd115a1ebbc1b6c2abe
doni-robert <irobertndungu@gmail.com>
2022-06-10 18:56:58 -0700

    correction made

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_h
 /**
 * Description: Header file containing prototypes for all functions
-* used in the 0x03-more_functions_nested_loops directory.
+* used in the 0x04-more_functions_nested_loops directory.
 */
 
 int _putchar(char c);
