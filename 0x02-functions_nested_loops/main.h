5e1e95537f0fdbd27e79bbd1f0bdcc36a062248f
doni-robert <irobertndungu@gmail.com>
2022-06-13 10:18:46 -0700

    task 3 done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- *contains declarations for all functions
- *used in the 0x02-functions_nested_loops directory
+ *contains declarations for all functions used in the 0x02-functions_nested_loops directory
  */
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
