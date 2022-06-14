db3e006737112b7742440fb757eedcc96ce67e28
doni-robert <irobertndungu@gmail.com>
2022-06-14 13:13:54 -0700

    task 8 done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@ void print_rev(char *s);
 int _putchar(char c);
 int _strlen(char *s);
 void rev_string(char *s);
-void puts2(char *str);
+void puts2(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
