cdb4bdafd0aa2cafbcc4a0065736ae6d1a3163c1
doni-robert <irobertndungu@gmail.com>
2022-06-14 12:32:27 -0700

    task 4 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - Gets the length of a string
+ *@str: The string to get the length of.
+ *
+ *Return: The length of the string
+ */
+
+size_t _strlen(const char *str)
+
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
