366e41bfaed31b9d71febac21ebb7cf928f6e867
doni-robert <irobertndungu@gmail.com>
2022-06-15 09:56:46 -0700

    task 11 done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from input to 98,
- * 
+ * print_to_98 - prints all natural numbers from input to 98
+ *
  * @n: The number where counting begins.
  */
 
