ace2130b4fc8f4a7d598251a7640116e66a4bdbf
doni-robert <irobertndungu@gmail.com>
2022-06-22 01:16:53 -0700

    fixed more betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
+ * _strlen_recursion - returns the length of a string
  *
+ *@s: string to be examined
  */
 int _strlen_recursion(char *s)
 {
 	int len = 0;
-	
 	if (*s)
 	{
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
-	return(len);
+	return (len);
 }
