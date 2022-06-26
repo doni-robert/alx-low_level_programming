250c8dab12547131d34648c437b8065cca471324
doni-robert <irobertndungu@gmail.com>
2022-06-25 20:42:29 -0700

    task 0 done

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - Gets the length of a string
+ *@s: The string to get the length of.
+ *
+ *Return: The length of the string
+ */
+
+int _strlen(char *s)
+
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
+}
