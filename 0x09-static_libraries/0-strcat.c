0600ac1c5f031a16f25d971c0a343f08c9a8e88a
doni-robert <irobertndungu@gmail.com>
2022-06-29 17:10:34 -0700

    corrected task 0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,16 +1,5 @@
 #include "main.h"
 
-/**
- *strcat - This function appends the src string to the dest string, 
- *overwriting the terminating null byte (\0) at the end of dest, 
- *and then adds a terminating null byte
- *
- *@dest: Pointer to the string to be concatenated
- *@src: The source string to be appended
- *
- *Return: A pointer to the destination string 
- */
-
 char *_strcat(char *dest, char *src)
 {
 	
