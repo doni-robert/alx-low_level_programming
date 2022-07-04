fc2fc7bdc0a3b5b52b1153370ba1100306c5f246
doni-robert <irobertndungu@gmail.com>
2022-07-04 08:59:39 -0700

    task 3 corrected

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/*
- * Desc: Defines function like macro
- *
- *
- */
 
-#define ABS(x) ((x) >= 0 ? (x) : (-(x)))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif 
