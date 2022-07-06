f579c3ff6efd76beed230335db2705d93d0ea495
doni-robert <irobertndungu@gmail.com>
2022-07-05 18:09:42 -0700

    tasks 0,1,2,3 and 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *
+ */
+
+dog_t *new_dog(char *name, float *age, char *owner)
+{
+	/*if (new_dog == NULL)
+        	return (NULL);
+		else*/
+	{
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
+	}
+
+}
