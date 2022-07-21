9e1194cbac380f34b545b1cc1d9506b6ddd0455f
doni-robert <irobertndungu@gmail.com>
2022-07-20 19:24:14 -0700

    corrected task 5 and 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_node_at_index - function that returns the nth node
+ * get_nodeint_at_index - function that returns the nth node
  *     of a listint_t linked list.
  *
  * @head: head of the list
