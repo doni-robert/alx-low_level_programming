f071b40fdba3db9880a5f6ec8db7cf87abea3db0
doni-robert <irobertndungu@gmail.com>
2023-01-09 20:39:33 +0300

    corrected betty dyntax

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,6 +11,6 @@
 
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-        printf("%i\n", argc - 1);
+	printf("%i\n", argc - 1);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
-	        printf("%i\n", mul);
-        	return (0);
+		printf("%i\n", mul);
+		return (0);
 	}
 	else
 	{
