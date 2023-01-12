51eb7a4dabd73204a083b09d03465c44efa7f93a
doni-robert <irobertndungu@gmail.com>
2023-01-12 15:11:30 +0300

    Updated free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,10 +23,12 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(width * sizeof(int));
+		ptr[i] = (int *)malloc(width * sizeof(int));
 
 		if (ptr[i] == NULL)
 		{
+			for (; i >= 0; i--)
+				free (ptr[i]);
 			free(ptr);
 			return (NULL);
 		}
