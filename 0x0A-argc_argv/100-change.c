e8191c2d3083d485d5a68f8c605a2b4110ad8cc8
doni-robert <irobertndungu@gmail.com>
2023-01-13 11:52:43 +0300

    task 5 complete

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,34 +13,29 @@
 int main(int argc, char *argv[])
 {
 	int arr[5] = {25, 10, 5, 2, 1};
-	int num, n, i, count = 0;
+	int i, n, count = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (n = 0; argv[1][n]; n++)
-	{
-		if (argv[1][n] < '0' || argv[1][n] > '9')
-			printf("0\n");
-	}
+	n = atoi(argv[1]);
 
-	num = atoi(argv[1]);
-
-	if (num > 0)
+	if (n > 0)
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (num / arr[i] >= 1)
+			if (n / arr[i] >= 1)
 			{
-				count += num / arr[i];
-				num = num % arr[i];
+				count += n / arr[i];
+				n = n % arr[i];
 			}
 		}
-		printf("%i\n", count);
+		printf("%d\n", count);
 	}
 	else
 		printf("0\n");
+
 	return (0);
 }
