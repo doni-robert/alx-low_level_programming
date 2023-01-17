b4cb285c4e9e3d4f476aa155c73efacc866a4a1d
doni-robert <irobertndungu@gmail.com>
2023-01-17 19:38:27 +0300

    corrected check

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -68,7 +68,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->owner = malloc(sizeof(char) * (_strlen(name) + 1));
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
