6e32e623e61a51fd6a817fe56df8f3b7d3c9c0ef
doni-robert <irobertndungu@gmail.com>
2023-01-18 11:34:02 +0300

    changed strcpy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -76,8 +76,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	 _strcpy(new_dog->name, name);
-	 _strcpy(new_dog->owner, owner);
+	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->owner =  _strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 
 	return (new_dog);
