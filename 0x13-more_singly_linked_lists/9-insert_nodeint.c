ac23116bde9e85892f7b25fb951f7042aa42b6c1
doni-robert <irobertndungu@gmail.com>
2023-02-03 16:29:50 +0300

    fix code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,22 +25,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = temp;
+		*head = new_node;
+		return (new_node);
+	}
 	for (;; idx--)
 	{
-		if (temp == NULL)
+		if (temp == NULL || temp->next == NULL)
 			return (NULL);
 		temp = temp->next;
 	}
-	if (temp->next == NULL)
-	{
-		new_node->next = NULL;
-		temp->next = new_node;
-	}
-	else
-	{
-		new_node->next = temp->next;
-		temp->next = new_node;
-	}
+
+	new_node->next = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
 
